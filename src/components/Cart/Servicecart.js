ed2881bffed03d2d4d83651663a7237cbf7e2f18
codeIriss <goal.matters.20@gmail.com>
2020-03-14 12:30:06 +0530

    web address changed

--- src/components/Cart/Servicecart.js
+++ src/components/Cart/Servicecart.js
@@ -39,7 +39,7 @@ export default class Servicecart extends Component {
                                                    data-item-id={node.id}
                                                    data-item-name={node.title}
                                                    data-item-price={node.price}
-                                                   data-item-url=""
+                                                   data-item-url="https://pristeen.netlify.com/"
                                                    data-item-image={node.image.fixed.src}
                                                    className="btn btn-warning snipcart-add-item ">
                                              order now
